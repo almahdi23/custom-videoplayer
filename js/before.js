9956fdd1b635b152ab15e4d660dabda5406402cc
almahdi23 <almahdi2345@gmail.com>
2019-08-15 23:26:18 -0700

    solved some glitches

--- js/before.js
+++ js/before.js
@@ -1,8 +1,18 @@
-window.onload = function(){
-	var c = document.cookie
-	var co = c.split("=");
-	var tf = co[1];
-	if(tf == "true"){
-		document.getElementById("autoplay").setAttribute("checked","");
-	}
-}
+window.onload = function () {
+    var cookies = document.cookie
+    var cookiesarray = cookies.split(";");
+    for (cook in cookiesarray) {
+        var cookie = cookiesarray[cook]
+        var cookiearray = cookie.split("=")
+        for (c in cookiearray) {
+            var cookiename = cookiearray[0]
+            if (cookiename == "ap") {
+                var cookievalue = cookiearray[1]
+                if (cookievalue == "true") {
+                    document.getElementById("autoplay").setAttribute("checked", "");
+                };
+            };
+        };
+    };
+
+};
